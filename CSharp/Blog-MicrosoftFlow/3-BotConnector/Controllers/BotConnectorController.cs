50bf080e5ca95a0756ffc8d60823fe1f9d82be8e
pablo costantini <pablo.costantini@southworks.com>
2017-06-19 12:41:52 -0300

    MicrosoftFlow Bot Connector

---
+++ CSharp/Blog-MicrosoftFlow/3-BotConnector/Controllers/BotConnectorController.cs
@@ -0,0 +1,98 @@
+﻿namespace FlowBotConnector
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    public class BotConnectorController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]BotConnectorEventRequest request)
+        {
+            try
+            {
+                var accessToken = await ObtainBotAccessToken(request.MicrosoftApplicationId, request.MicrosoftApplicationPassword);
+
+                await SendEventToBot(request.BotEndpointUri, accessToken, request.EventActivityValue);
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.ToString());
+            }
+        }
+
+        private static async Task<string> ObtainBotAccessToken(string applicationId, string applicationPassword)
+        {
+            const string loginUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+
+            var body = new List<KeyValuePair<string, string>>();
+            body.Add(new KeyValuePair<string, string>("grant_type", "client_credentials"));
+            body.Add(new KeyValuePair<string, string>("scope", "https://graph.microsoft.com/.default"));
+            body.Add(new KeyValuePair<string, string>("client_id", applicationId));
+            body.Add(new KeyValuePair<string, string>("client_secret", applicationPassword));
+
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage(HttpMethod.Post, loginUrl) { Content = new FormUrlEncodedContent(body) };
+                var response = await client.SendAsync(request);
+
+                var json = await response.Content.ReadAsStringAsync();
+                var jobject = JObject.Parse(json);
+
+                var accessToken = jobject.Value<string>("access_token");
+                if (string.IsNullOrEmpty(accessToken))
+                {
+                    var error = jobject.Value<string>("error");
+                    var description = jobject.Value<string>("error_description");
+                    throw new ArgumentException(string.Format("The provided ApplicationId and Password did not generate a valid token: {0}, {1}", error, description));
+                }
+
+                return accessToken;
+            }
+        }
+
+        private static async Task SendEventToBot(string botEndpointUri, string accessToken, dynamic eventActivityPayload)
+        {
+            var eventActivity = new
+            {
+                Type = "event",
+                ChannelId = "flow",
+                Value = eventActivityPayload
+            };
+
+            using (var client = new HttpClient())
+            {
+                var content = new StringContent(
+                    JsonConvert.SerializeObject(eventActivity),
+                    Encoding.UTF8,
+                    "application/json");
+
+                client.DefaultRequestHeaders.Add("authorization", string.Format("Bearer {0}", accessToken));
+
+                var response = await client.PostAsync(botEndpointUri, content);
+            }
+        }
+    }
+
+    public class BotConnectorEventRequest
+    {
+        public string MicrosoftApplicationId { get; set; }
+
+        public string MicrosoftApplicationPassword { get; set; }
+
+        public string BotEndpointUri { get; set; }
+
+        public dynamic EventActivityValue { get; set; }
+    }
+}
---
+++ CSharp/Blog-MicrosoftFlow/3-BotConnector/Controllers/MessagesController.cs
@@ -0,0 +1,72 @@
+﻿namespace FlowBotConnector
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using Microsoft.Bot.Connector;
+    using Newtonsoft.Json.Linq;
+
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            switch (activity.Type)
+            {
+                case ActivityTypes.Message:
+                    // await Conversation.SendAsync(activity, () => new RootDialog());
+                    break;
+
+                case ActivityTypes.Event:
+                    var value = activity.Value as JObject;
+
+                    // Here we convert our message to a .NET type
+                    // e.g.:
+                    // var message = value.ToObject<MyMessage>();
+
+                    break;
+
+                default:
+                    this.HandleSystemMessage(activity);
+                    break;
+            }
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
