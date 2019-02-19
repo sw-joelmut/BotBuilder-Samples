da36957c553e261ff3156e59c6ff980705c63b48
Billy Delgado <37461749+Bill7zz@users.noreply.github.com>
2019-02-19 09:24:55 -0300

    Update main view and endpoint view with button command

---
+++ generators/vsix-vs-win/BotConfigurationWizard/BotFileCreator/Views/EndpointView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BotFileCreator
+{
+    /// <summary>
+    /// Interaction logic for EndpointView.xaml
+    /// </summary>
+    public partial class EndpointView
+    {
+        public EndpointView()
+        {
+            var endpointViewModel = new EndpointViewModel();
+            endpointViewModel.CloseAction = new Action(() => this.Close());
+            DataContext = endpointViewModel;
+            InitializeComponent();
+        }
+    }
+}
