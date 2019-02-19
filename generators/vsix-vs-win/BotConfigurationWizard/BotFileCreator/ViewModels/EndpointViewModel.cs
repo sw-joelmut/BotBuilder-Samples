d33632365a00a93a396af6b0731a95871ba25995
Billy Delgado <37461749+Bill7zz@users.noreply.github.com>
2019-02-19 09:22:01 -0300

    Add endpoint view model class

---
+++ generators/vsix-vs-win/BotConfigurationWizard/BotFileCreator/ViewModels/EndpointViewModel.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Windows.Input;
+using BotFileCreator.Repository;
+
+namespace BotFileCreator
+{
+    public class EndpointViewModel : BaseViewModel
+    {
+        private IBotConfigurationRepository _repository;
+
+        private readonly ICommand _cancelCommand;
+
+        private readonly ICommand _submitCommand;
+
+        private EndpointItem _endpointItem;
+
+        public EndpointViewModel()
+        {
+            _endpointItem = new EndpointItem();
+            _cancelCommand = new RelayCommand(param => this.CloseAction(), null);
+            _submitCommand = new RelayCommand(param => this.Submit(), null);
+        }
+
+        public Action CloseAction { get; set; }
+
+        public EndpointItem EndpointItem { get => _endpointItem; set => SetProperty(ref _endpointItem, value); }
+
+        public ICommand CancelCommand { get => _cancelCommand; }
+
+        public ICommand SubmitCommand { get => _submitCommand; }
+
+        private void Submit()
+        {
+        }
+    }
+}
