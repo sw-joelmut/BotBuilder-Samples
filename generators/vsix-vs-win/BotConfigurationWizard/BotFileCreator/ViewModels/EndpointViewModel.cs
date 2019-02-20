7317973fe7bcdceb5c3eba5754d51dbd9a9b9025
Billy Delgado <37461749+Bill7zz@users.noreply.github.com>
2019-02-20 11:41:14 -0300

    Add endpoint validations

--- generators/vsix-vs-win/BotConfigurationWizard/BotFileCreator/ViewModels/EndpointViewModel.cs
+++ generators/vsix-vs-win/BotConfigurationWizard/BotFileCreator/ViewModels/EndpointViewModel.cs
@@ -4,6 +4,8 @@
 namespace BotFileCreator
 {
     using System;
+    using System.Text.RegularExpressions;
+    using System.Windows;
     using System.Windows.Input;
 
     public class EndpointViewModel : BaseViewModel
@@ -14,27 +16,66 @@ namespace BotFileCreator
 
         private readonly ICommand _submitCommand;
 
-        private EndpointService _endpointItem;
+        private EndpointService _endpointService;
 
         public EndpointViewModel()
         {
             _repository = SettingsRepository.GetInstance();
-            _endpointItem = new EndpointService();
+            _endpointService = new EndpointService();
             _cancelCommand = new RelayCommand(param => this.CloseAction(), null);
             _submitCommand = new RelayCommand(param => this.Submit(), null);
         }
 
         public Action CloseAction { get; set; }
 
-        public EndpointService EndpointItem { get => _endpointItem; set => SetProperty(ref _endpointItem, value); }
+        public EndpointService EndpointService { get => _endpointService; set => SetProperty(ref _endpointService, value); }
 
         public ICommand CancelCommand { get => _cancelCommand; }
 
         public ICommand SubmitCommand { get => _submitCommand; }
 
+        public Tuple<bool, string> EndpointServiceIsValid()
+        {
+            var urlRegex = new Regex(@"((http)(s)?:\/\/)", RegexOptions.IgnoreCase);
+
+            if (string.IsNullOrWhiteSpace(EndpointService.Name))
+            {
+                return new Tuple<bool, string>(false, "Endpoint name can't be null.");
+            }
+
+            if (string.IsNullOrWhiteSpace(EndpointService.Endpoint))
+            {
+                return new Tuple<bool, string>(false, "Endpoint URL can't be null.");
+            }
+
+            if (!urlRegex.IsMatch(this.EndpointService.Endpoint))
+            {
+                return new Tuple<bool, string>(false, "Please choose a valid endpoint URL. \nInclude route if necessary: /api/messages");
+            }
+
+            return new Tuple<bool, string>(true, string.Empty);
+        }
+
         private void Submit()
         {
-            _repository.ConnectService(this.EndpointItem);
+            // Checks if the endpoint service is valid
+            Tuple<bool, string> endpointIsValid = EndpointServiceIsValid();
+
+            if (!endpointIsValid.Item1)
+            {
+                MessageBox.Show(endpointIsValid.Item2, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(EndpointService.Id))
+            {
+                _repository.ConnectService(this.EndpointService);
+            }
+            else
+            {
+                _repository.EditService(this.EndpointService);
+            }
+
             this.CloseAction();
         }
     }
