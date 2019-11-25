31540f4d319469ff204050be4dc1e5888ea5a3a8
matiasroldan6 <matias.roldan@southworks.com>
2019-11-25 10:53:34 -0300

    Add MultiTurnPromptBot and CardsBot

--- samples/csharp_dotnetcore/06.using-cards/Program.cs
+++ samples/csharp_dotnetcore/06.using-cards/Program.cs
@@ -1,9 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 
 namespace Microsoft.BotBuilderSamples
 {
@@ -11,16 +10,15 @@ namespace Microsoft.BotBuilderSamples
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .ConfigureLogging((logging) =>
-                {
-                    logging.AddDebug();
-                    logging.AddConsole();
-                })
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+                    Host.CreateDefaultBuilder(args)
+                        .ConfigureWebHostDefaults(webBuilder =>
+                        {
+                            webBuilder
+                                .UseStartup<Startup>();
+                        });
     }
 }
