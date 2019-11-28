6c0acf5f2348e8ebfbf571128db9956807b77fa4
Luis Sergio <37625424+LCarbonellRod@users.noreply.github.com>
2019-11-28 17:11:17 -0300

    Update TeamsLinkUnfurlingBot Sample
    -Framework NET Core from 2.1 to 3.0
    
    -Startup.cs
    
    -Program.cs

--- samples/csharp_dotnetcore/55.teams-link-unfurling/Startup.cs
+++ samples/csharp_dotnetcore/55.teams-link-unfurling/Startup.cs
@@ -3,7 +3,7 @@
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.BotBuilderSamples.Bots;
@@ -24,7 +24,7 @@ namespace Microsoft.BotBuilderSamples
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddRazorPages();
 
             // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
             services.AddSingleton<IStorage, MemoryStorage>();
@@ -37,7 +37,7 @@ namespace Microsoft.BotBuilderSamples
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -48,11 +48,15 @@ namespace Microsoft.BotBuilderSamples
                 app.UseHsts();
             }
 
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseDefaultFiles();
             app.UseStaticFiles();
-
-            //app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
