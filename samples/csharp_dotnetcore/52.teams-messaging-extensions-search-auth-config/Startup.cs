6374b3ef8de036b7142c6b6bc892bcb6d8fddef2
Luis Sergio <37625424+LCarbonellRod@users.noreply.github.com>
2019-11-29 12:00:08 -0300

    Update TeamsMessagingExtensionsSearchAuthConfig Sample
    -Framework NET Core from 2.1 to 3.0
    
    -Startup.cs
    
    -Program.cs

--- samples/csharp_dotnetcore/52.teams-messaging-extensions-search-auth-config/Startup.cs
+++ samples/csharp_dotnetcore/52.teams-messaging-extensions-search-auth-config/Startup.cs
@@ -3,7 +3,7 @@
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Extensions.Configuration;
@@ -25,7 +25,7 @@ namespace Microsoft.BotBuilderSamples
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddRazorPages();
 
             // Create storage and UserState for User configuration settings.
             services.AddSingleton<IStorage>(new MemoryStorage());
@@ -39,7 +39,7 @@ namespace Microsoft.BotBuilderSamples
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -50,11 +50,15 @@ namespace Microsoft.BotBuilderSamples
                 app.UseHsts();
             }
 
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseDefaultFiles();
             app.UseStaticFiles();
-
-            //app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
