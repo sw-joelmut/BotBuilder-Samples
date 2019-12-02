f556ab8e3a853cc6a0b82fdce91418f0c245e997
Luis Sergio <37625424+LCarbonellRod@users.noreply.github.com>
2019-12-02 15:59:07 -0300

    Update TwilioTestbot to NetCore 3.0

--- samples/csharp_dotnetcore/52.twilio-adapter/Program.cs
+++ samples/csharp_dotnetcore/52.twilio-adapter/Program.cs
@@ -1,22 +1,26 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-//
-// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.3.0
 
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using TwilioAdapterBot;
 
-namespace TwilioAdapterBot
+namespace Microsoft.BotBuilderSamples
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+                    Host.CreateDefaultBuilder(args)
+                        .ConfigureWebHostDefaults(webBuilder =>
+                        {
+                            webBuilder
+                                .UseStartup<Startup>();
+                        });
     }
 }
+
--- samples/csharp_dotnetcore/52.twilio-adapter/Startup.cs
+++ samples/csharp_dotnetcore/52.twilio-adapter/Startup.cs
@@ -5,12 +5,13 @@
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Adapters.Twilio;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Extensions.DependencyInjection;
 using TwilioAdapterBot.Bots;
+using Microsoft.Extensions.Logging;
 
 namespace TwilioAdapterBot
 {
@@ -19,17 +20,21 @@ namespace TwilioAdapterBot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddRazorPages();
 
             // Create the Bot Framework Twilio Adapter.
             services.AddSingleton<IBotFrameworkHttpAdapter, TwilioAdapter>();
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
             services.AddTransient<IBot, EchoBot>();
+
+            var serviceProvider = services.BuildServiceProvider();
+            var logger = serviceProvider.GetService<ILogger<TwilioAdapter>>();
+            services.AddSingleton(typeof(ILogger), logger);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -40,11 +45,15 @@ namespace TwilioAdapterBot
                 app.UseHsts();
             }
 
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseDefaultFiles();
             app.UseStaticFiles();
-
-            // app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
